8b47761f9cffd26880815de712490a6015219502
Bowie2k3 <antonio@antlis.com>
2016-09-19 20:03:48 -0400

    Initial commit

---
+++ COS-205_assignment6_AJ.py
@@ -0,0 +1,26 @@
+# Write a program that accepts a speed limit and a clocked speed from the user,
+# and either prints a message indicating the speed was legal or prints the amount
+# of the fine, if the speed was illegal.
+
+
+def speeding(limit, clocked):
+    fine = 0
+    if limit >= clocked:
+        print("Speed was legal.")
+    else:
+        if limit < clocked:
+            # The speeding ticket fine policy in Podunskville is $50 plus $5 for each mph over
+            fine = (clocked - limit) * 5
+    # the speed limit plus a penalty of $200 for any speed over 90 mph
+    if clocked > 90:
+        fine += 200
+    print("The amount of the fine is $" + str(fine))
+
+
+def main():
+    limit = eval(input("Please enter posted speed limit: "))
+    speed = eval(input("Please enter clocked speed: "))
+    speeding(limit, speed)
+
+main()
+
