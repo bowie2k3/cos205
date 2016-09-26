9880d7aa38e3f1d80435ce33c749c4948e1d0d0b
Bowie2k3 <antonio@antlis.com>
2016-09-25 21:12:32 -0400

    Initial commit

--- COS-205_assignment7_AJ.py
+++ COS-205_assignment7_AJ.py
@@ -1,32 +1,34 @@
-"""The greatest common divisor (GCD) of two values can be computed using Euclid’s algorithm.
-Starting with the values of n and m where n > m, we repeatedly apply the formula:
-
-n, m=m, n%m (i.e., n is replaced by m, and m is replaced by n%m) until m is 0.
-At that point, n is the GCD of the original n and m.
-
-Your program should prompt the user for two positive whole numbers (i.e., 1, 2, 3, …).
-If the user enters a zero or negative number, your program should display an error message
-and keep prompting until a valid number is entered.
-
-After computing a GCD, your program should ask the user if he or she would like
-to do another computation or not. If the user enters Yes or Y (case insensitive),
-it should do another computation; and if the user enters No or N (case insensitive),
-it should terminate execution.
-"""
+# COS-205_assignment7_AJ.py
+# Euclid's algorithm n, m=m, n%m (i.e., n is replaced by m, and m is replaced by n%m) until m is 0.
 
 
-def gcd():
-    result = 0
+def gcd(n, m):
+        while m:
+            n, m = m, n % m
+        return n
+
+
+def main():
+    error = "The number entered is not a positive whole number!"
     while True:
-        n = eval(input("Please enter a positive whole number: "))
-        m = eval(input("Please enter a second whole number: "))
-        if n > 0 and m > 0:
-            while m > 0:
-                n = m
-                m = n % m
-            print("The GCD of ", n, "and", m, "is", result)
-        else:
-            print("The number entered is not a positive whole number!")
+        n, m = 0, 0
+        # prompt user for two positive whole numbers
+        # if the user enters a zero or negative number, display an error
+        while n <= 0 or n % 1 != 0:
+            n = eval(input("\nEnter a positive whole number: "))
+            if n <= 0 or n % 1 != 0:
+                print(error)
+        while m <= 0 or m % 1 != 0:
+            m = eval(input("Enter a second positive whole number: "))
+            if m <= 0 or m % 1 != 0:
+                print(error)
+        print("\nThe GCD of", n, "and", m, "is", gcd(n, m))
+        ans = input("\nCompute another GCD (Y/N): ").lower()
+        if ans in ["no", "n"]:
+            print("\nGoodbye")
+            break
+        if ans in ["yes", "y"]:
+            continue
 
 
-gcd()
+main()
