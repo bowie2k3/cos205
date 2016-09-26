ecb88691c5a06f22ba8ba703ff0f17a0b496d60c
Bowie2k3 <antonio@antlis.com>
2016-09-26 12:32:13 -0400

    Initial commit

--- COS-205_assignment7_AJ.py
+++ COS-205_assignment7_AJ.py
@@ -1,34 +1,50 @@
 # COS-205_assignment7_AJ.py
-# Euclid's algorithm n, m=m, n%m (i.e., n is replaced by m, and m is replaced by n%m) until m is 0.
 
 
 def gcd(n, m):
-        while m:
-            n, m = m, n % m
-        return n
+    while m:
+        n, m = m, n % m
+    return n
+
+
+def is_whole(x):
+    error = "The number entered is not a positive whole number!"
+    # Check that a number is positive and whole
+    if x > 0 and x % 1 == 0:
+        return True
+    # if the user enters a zero or negative number, display an error
+    elif x < 0 or x % 1 != 0:
+        print(error)
 
 
 def main():
     error = "The number entered is not a positive whole number!"
-    while True:
-        n, m = 0, 0
-        # prompt user for two positive whole numbers
-        # if the user enters a zero or negative number, display an error
-        while n <= 0 or n % 1 != 0:
-            n = eval(input("\nEnter a positive whole number: "))
-            if n <= 0 or n % 1 != 0:
-                print(error)
-        while m <= 0 or m % 1 != 0:
-            m = eval(input("Enter a second positive whole number: "))
-            if m <= 0 or m % 1 != 0:
-                print(error)
-        print("\nThe GCD of", n, "and", m, "is", gcd(n, m))
-        ans = input("\nCompute another GCD (Y/N): ").lower()
-        if ans in ["no", "n"]:
-            print("\nGoodbye")
-            break
-        if ans in ["yes", "y"]:
-            continue
+    try:
+        while True:
+            n, m = 0, 0
+            # prompt user for two positive whole numbers
+            print()
+            while not is_whole(n):
+                n = eval(input("Enter a positive whole number: "))
+                if n == 0:
+                    print(error)
+            while not is_whole(m):
+                m = eval(input("Enter a second positive whole number: "))
+                if m == 0:
+                    print(error)
+            print("\nThe GCD of", n, "and", m, "is", gcd(n, m))
+            # After computing a GCD, your program should ask the user if he or she would like to do
+            # another computation or not. If the user enters Yes or Y (case insensitive),
+            ans = input("\nCompute another GCD (Y/N): ").lower()
+            if ans in ["no", "n"]:
+                print("\nGoodbye!")
+                break
+            if ans in ["yes", "y"]:
+                continue
+    except NameError:
+        print("Sorry, that was not a valid number.  Please try again.")
+    except SyntaxError:
+        print("Sorry, that was not a valid number.  Please try again.")
 
 
 main()
